a5051d16a7a7ce33f7094d0972f34da5c0270517
Vontell <sankarsh.rajeev@gmail.com>
2023-09-20 18:04:05 -0700

    files added

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,20 +2,30 @@ import { Typography } from '@mui/material';
 import React from 'react';
 
 const Card = (props) => (
-  <div className="card">
-    <img src={props.imgUrl} alt={props.alt || 'Image'} />
-    <div className="card-content">
-      <h2>{props.title}</h2>
-      <p>{props.content}</p>
+  <a href={props.url} target="_blank" rel="noopener noreferrer">
+    <div className="card">
+      <img src={props.imgUrl} alt={props.alt || 'Image'} />
+      <div className="card-content">
+        <h2 className='text-bold'>{props.title}</h2>
+        <p>{props.content}</p>
+        
+      </div>
     </div>
-  </div>
+  </a>
 );
 
 const CardContainer = (props) => (
-  <div className="cards-container">
+  <div className="cards-container ">
     {
       props.cards.map((card) => (
-        <Card title={card.title} content={card.content} imgUrl={card.imgUrl} key={card.id} />
+        <Card 
+          title={card.title} 
+          url={card.url}
+          content={card.content} 
+          imgUrl={card.imgUrl} 
+          technologies={card.technologies}
+          key={card.id} 
+        />
       ))
     }
   </div>
@@ -23,38 +33,39 @@ const CardContainer = (props) => (
 
 export default function Portfolio() {
   const frontendProjects = [
-    {id: 1, title: 'Project 1', content: 'HTML, CSS', imgUrl: 'https://unsplash.it/200/200'},
-    {id: 2, title: 'Project 2', content: 'JavaScript', imgUrl: 'https://unsplash.it/201/200'},
-    {id: 2, title: 'Project 2', content: 'JavaScript', imgUrl: 'https://unsplash.it/201/200'},
-    {id: 2, title: 'Project 2', content: 'JavaScript', imgUrl: 'https://unsplash.it/201/200'},
-    {id: 2, title: 'Project 2', content: 'JavaScript', imgUrl: 'https://unsplash.it/201/200'},
-    {id: 2, title: 'Project 2', content: 'JavaScript', imgUrl: 'https://unsplash.it/201/200'},
-    /* add more */
+    {id: 3, title: 'Note Taker', content: 'A Front-End Application', imgUrl: '/assets/port_assets/notes.jpeg',url:"https://shocking-zombie-29357.herokuapp.com/"},
+    {id: 5, title: 'Work Day Scheduler', content: 'A Front-End Application', imgUrl: '/assets/port_assets/workday.jpeg',url:"https://sankyrajeev.github.io/work-day-scheduler/"},
+    {id: 4, title: 'Weather Dashboard', content: 'A Front-End Application', imgUrl: '/assets/port_assets/weather.jpeg',url:"https://sankyrajeev.github.io/Weather-Dashboard/"},
+    {id: 1, title: 'Travelite', content: 'A Travel App', imgUrl: '/assets/port_assets/travel.jpeg', url:"https://travelite.herokuapp.com/#/"},
+    {id: 2, title: 'Washoe', content: 'A Resposive Web Design page', imgUrl: '/assets/port_assets/washoe.jpeg',url:"https://sankyrajeev.github.io/washoe-final/"},
+    // add more
   ];
 
   const fullstackProjects = [
-    {id: 3, title: 'Project A', content: 'Node.js, MongoDB', imgUrl: 'https://unsplash.it/200/201'},
-    {id: 4, title: 'Project B', content: 'Python, Django', imgUrl: 'https://unsplash.it/201/201'},
-    /* add more */
+    {id: 2, title: 'Washoe', content: 'A Resposive Web Design page', imgUrl: 'https://unsplash.it/201/200',url:"https://sankyrajeev.github.io/washoe-final/"},
+    {id: 4, title: 'Project B', content: 'An e-commerce site', imgUrl: 'https://unsplash.it/201/201', },
+    // add more
   ];
 
   const musicLinks = [
-    /* Populate with music Spotify Links */
+    // Populate with music Spotify Links
   ];
 
   return (
-    
+    <div
+        className="bg-cover bg-center  h-screen flex items-center justify-center"
+        style={{ backgroundImage: `url('/assets/rm222batch5-mind-10.jpg')` }}
+      >
     <div className="container">
-      {/* <Typography className='mt-5 pb-5 text-center font-bold' style={{textAlign: 'center'}} variant="h2"  sx={{ fontFamily: 'Croissant One'}}>My Portfolio</Typography> */}
-      
-      <Typography className='mt-5 mb-5 text-center font-bold' variant="h4"  sx={{ fontFamily: 'Croissant One'}}>Front End Development</Typography>
-      <CardContainer cards={frontendProjects} />
+      <Typography className='mt-5 mb-5 text-center font-bold' variant="h4"  sx={{ fontFamily: 'Croissant One'}}>My Projects</Typography>
+      <CardContainer className="mb-5" cards={frontendProjects} />
 
-      <Typography className='mt-5 mb-5 text-center font-bold' variant="h4"  sx={{ fontFamily: 'Croissant One'}}>Full Stack Development</Typography>
-      <CardContainer cards={fullstackProjects} />
+      {/* <Typography className='mt-5 mb-5 text-center font-bold' variant="h4"  sx={{ fontFamily: 'Croissant One'}}>Full Stack Projects</Typography>
+      <CardContainer cards={fullstackProjects} /> */}
 
-      <h2>Music</h2>
-      {/* Render your music Spotify links here */}
+      {/* <h2>Music</h2>
+      Render your music Spotify links here */}
+    </div>
     </div>
   );
 }
